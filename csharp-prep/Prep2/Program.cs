55e39cc83bb4687066f2c8733094a73830835fb1
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-22 21:36:09 -0700

    added grades

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int gpercent = int.Parse(response);
+
+        if (gpercent >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (gpercent >= 80);
+        {
+            grade = "B";
+        }
+        
+        else if (gpercent >= 70);
+        {
+            grade = "C";
+        }
+
+        else if (gpercent >= 60);
+        {
+            grade = "D";
+        }
+
+        else if (gpercent < 60);
+        {
+            grade = "F";
+        }
+
     }
 }
