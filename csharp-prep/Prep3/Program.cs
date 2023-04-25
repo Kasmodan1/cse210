df3dd4a3e25407629f656533feddeaa04f0fddf1
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-24 21:39:33 -0700

    adding IF statment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+       Console.Write("What is the magic number?:");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your Guess?:");
+        int guessNumber = int.Parse(Console.ReadLine());
+
+        if (magicNumber == guessNumber)
+            Console.Write("You guessed the Magic Number!");
+
+        else if (magicNumber > guessNumber)
+            Console.Write("Higher");
+        
+        else if (magicNumber < guessNumber)
+            Console.Write("Lower");
+
+
     }
 }
