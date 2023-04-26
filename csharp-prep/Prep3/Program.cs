ebd9276fc3f98946dd987da1d73e312f5ae3e3de
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-25 22:49:27 -0700

    added the loop part

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,21 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-       Console.Write("What is the magic number?:");
+        Console.Write("What is the magic number?:");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your Guess?:");
-        int guessNumber = int.Parse(Console.ReadLine());
+        int guessNumber = 0;
 
-        if (magicNumber == guessNumber)
-            Console.Write("You guessed the Magic Number!");
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your Guess?:");
+            guessNumber = int.Parse(Console.ReadLine());
 
-        else if (magicNumber > guessNumber)
-            Console.Write("Higher");
-        
-        else if (magicNumber < guessNumber)
-            Console.Write("Lower");
-
+            if (magicNumber == guessNumber)
+            {
+                Console.WriteLine("You guessed the Magic Number!");
+            }
+            else if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
 
     }
 }
