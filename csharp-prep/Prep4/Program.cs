fa38b5628919e0091941d74ffd4564c7bd74bc05
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-25 23:40:27 -0700

    Adding Do/while

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbersList = new List<int>();
+
+        do {
+            Console.Write("Enter a list of numbers, type 0 when finished.");
+            int numbers = int.Parse(Console.ReadLine());
+                if (numbers != 0) {
+                    numbersList.Add(numbers);
+                }
+        } while (numbers != 0);
+            foreach (int number in numbers) {
+                Console.WriteLine(number);
+            }
+        
     }
 }
