7f0c35f02e3239a11ae955bb6cca2bdd3ef98b74
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-26 19:05:46 -0700

    added 2 functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        
+        static void PromptUserName()
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = (Console.ReadLine());
+        }
+        
+        
         Console.WriteLine("Hello Prep5 World!");
     }
 }
