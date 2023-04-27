a9009310e4e8237daf64dfcbf02917fd67596a52
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-26 20:29:29 -0700

    removed fuctions out of Main fuction

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,37 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        static void DisplayWelcome()
-        {
-            Console.WriteLine("Welcome to the program!");
-        }
-        
-        static string PromptUserName()
-        {
-            Console.WriteLine("Please enter your name:");
-            string name = Console.ReadLine();
 
-            return name;
-        }
-        
-        static int PromptUserNumber()
-        {
-            Console.WriteLine("Please enter your favorite number:");
-            int number = Console.ReadLine();
-
-        }
-
-        static int SquareNumber()
-        {
-            Console.WriteLine($"{PromptUserNumber}, the square of your number is {PromptUserNumber}");
-        }
-
-        static void DisplayResults()
-        {
-            {PromptUserName}
-        }
-
-
-        Console.WriteLine("Hello Prep5 World!");
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+        
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+        
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+
+        return square;
+    }
+
+    static void DisplayResults(string name, int square)
+    {
+        Console.WriteLine($"{name], the square of your number is {square}");
+    }
+
 }
