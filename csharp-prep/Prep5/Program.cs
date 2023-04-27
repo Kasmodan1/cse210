b025a578957a0edc9ac3a7202d1774d02cb2882c
Kasmodan1 <Kasmodan1@yahoo.com>
2023-04-26 20:18:34 -0700

    pushing bits and pieces of the funtions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -9,13 +9,32 @@ class Program
             Console.WriteLine("Welcome to the program!");
         }
         
-        static void PromptUserName()
+        static string PromptUserName()
         {
             Console.WriteLine("Please enter your name:");
-            string name = (Console.ReadLine());
+            string name = Console.ReadLine();
+
+            return name;
         }
         
-        
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number:");
+            int number = Console.ReadLine();
+
+        }
+
+        static int SquareNumber()
+        {
+            Console.WriteLine($"{PromptUserNumber}, the square of your number is {PromptUserNumber}");
+        }
+
+        static void DisplayResults()
+        {
+            {PromptUserName}
+        }
+
+
         Console.WriteLine("Hello Prep5 World!");
     }
 }
