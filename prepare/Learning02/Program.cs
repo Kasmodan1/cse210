ff632a186e1d2ff5da843825111b452bca802a38
Kasmodan1 <Kasmodan1@yahoo.com>
2023-05-04 16:22:35 -0700

    added job stuff

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = "";
+        job1._endYear = "";
+        
+        job2._jobTitle = "";
+        job2._company = "";
+        job2._startYear = "";
+        job2._endYear = "";
+    
     }
 }
---
+++ prepare/Learning02/resume.cs
@@ -0,0 +1,22 @@
+using System;
+
+  public class Resume 
+  {
+    //Class: Resume
+    //Responsibilities:
+    //Keeps track of the person's name and a list of their jobs.
+    //Behaviors:
+    //Displays the resume, which shows the name first, followed by displaying each one of the jobs.
+
+    public string _name;
+    
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+      Console.Write($"");
+    }
+    
+
+  }
+
