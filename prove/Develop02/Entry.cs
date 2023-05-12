3d76aebb531374939191871e5c12341db1d404bd
Kasmodan1 <Kasmodan1@yahoo.com>
2023-05-11 23:10:13 -0700

    figured out how to return my selection back to main program.

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -2,21 +2,23 @@ using System;
 
 public class Entry
 {
-    public void enteredSelection()
-    {
-      // These are the list of options
-      string[] entrys = {"1. Write a entry.", "2. Display current entrys.", "3. Load saved entrys.", "4. Save entry.", "5. Quit program." };
-
-      //show the options
-      Console.Write("Please select from the following options:");
-
-      for (int display = 0; display < entrys.Length; display++)
-        {
-          Console.WriteLine(entrys[display]);
-        }
-
-      Console.Write(":");
-      string input = Console.ReadLine();
-
-    }
+  public int EnteredSelection()
+  {
+    // These are the list of options.
+    string[] entrys = { "1. Write a entry.", "2. Display current entrys.", "3. Load saved entrys.", "4. Save entry.", "5. Quit program." };
+    //show the options.
+    Console.WriteLine("Please select from the following options:");
+    for (int display = 0; display < entrys.Length; display++)
+      {
+        //go though each and display it adding 1 to display until it reaches the length of array.
+        Console.WriteLine(entrys[display]);
+      }
+    Console.WriteLine(":");
+    // gather what was entered.
+    string input = Console.ReadLine();
+    //convert string to int.
+    int numberChoice = int.Parse(input);
+    //send back the int to the main program.
+    return numberChoice;
+  }
 }
