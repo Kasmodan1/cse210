f44ef0bc24e229c2919d3d2da092135942bd0a59
Kasmodan1 <Kasmodan1@yahoo.com>
2023-05-13 15:19:31 -0700

    just added some notes.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -39,9 +39,11 @@ public class Journal
 
     public void LoadJournalEntries()
     {
+        //creates the file
         string filename = "journal.txt";
         string[] lines = System.IO.File.ReadAllLines(filename);
 
+        // loop though each line item and mapping the parts.
         foreach (string line in lines)
         {
             string[] parts = line.Split("|");
@@ -50,7 +52,10 @@ public class Journal
             string prompt = parts[1];
             string response = parts[2];
 
+            // using the JournalEntry class to map it back to date,prompt,response
             JournalEntry entry = new JournalEntry(date, prompt, response);
+
+            // This should add it back into the entry list.
             AddJournalEntries(entry);
         }
 
