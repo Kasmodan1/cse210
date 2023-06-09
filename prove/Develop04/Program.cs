add41f52ba74edc2a88a1c5a69dfa113b4eda7e5
Kasmodan1 <Kasmodan1@yahoo.com>
2023-06-08 20:16:28 -0700

    pushing starting stuff.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        bool exit = false;
+
+    
+        while (!exit);
+        {
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+        Console.WriteLine("Select a number choice from the menu: ");
+        
+        string menuChoice = Console.ReadLine();
+
+            switch (menuChoice)
+            {
+                case "1":
+                    Console.WriteLine("Welcome to the Breathing Activity.");
+                    //start the breathing activity
+                    break;
+
+                case "2":
+                    Console.WriteLine("Welcome to the Reflection Activity.");
+                    //start the reflection activity.
+                    break;
+
+                case "3":
+                    Console.WriteLine("Welcome to the Listing Activity.");
+                    //start the listing activity
+                    break;
+                
+                case "4":
+                    // This will exit the while loop.
+                    exit = true;
+                    break;
+    
+                default:
+                    Console.WriteLine("Please enter a number from the menu.");
+                    break;
+
+            }
+
+            Console.WriteLine();
+
+        }
+        Console.WriteLine("Exiting the program. Press any key to close...");
+        // thought this was cool so they can hit any key to quit.
+        Console.ReadKey();
+ 
     }
+
 }
---
+++ prove/Develop04/activity.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Activity
+{
+    private int _startTime;
+    private int _endTime;
+    private int _duration;
+
+ 
+    
+
+ // -- Functions --
+ 
+    // This function displays the starting message and activity message.
+
+    // Welcome to the Breathing Activity.
+
+    // The description of this activity should be something like: "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing."
+    
+    public string DisplayMsg(params string[] strings)
+    {
+
+    }
+
+
+    public int ActivityDuration ()
+    {
+        Console.WriteLine("");
+    }
+
+
+    public int ActivityClearTime ()
+    {
+
+    }
+    
+    public int ActivityPauseTime ()
+    {
+
+    }
+
+}
