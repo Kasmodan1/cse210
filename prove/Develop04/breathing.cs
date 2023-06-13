77c7a60b9fef7ca30706092f73bfa227e66e2210
Kasmodan1 <Kasmodan1@yahoo.com>
2023-06-12 19:48:45 -0700

    last changes to breathing

--- prove/Develop04/breathing.cs
+++ prove/Develop04/breathing.cs
@@ -9,7 +9,6 @@ class Breathing : Activity
 
 //1.The activity should begin with the standard starting message and prompt for the duration that is used by all activities.
 //2. The description of this activity should be something like: "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing."
-
 //3. After the starting message, the user is shown a series of messages alternating between "Breathe in..." and "Breathe out..."
 //4. After each message, the program should pause for several seconds and show a countdown.
 //5. It should continue until it has reached the number of seconds the user specified for the duration.
@@ -58,17 +57,20 @@ class Breathing : Activity
 
             if (startTime + duration <= endTime)
             {
-                base.ActivityCountTime(3, "Now breath in...{0}");
+                base.ActivityCountTime(2, "Now breath in...{0}");
 
-                base.ActivityCountTime(5, "Now breath out...{0}");
+                base.ActivityCountTime(4, "Now breath out...{0}");
             }
                
             startTime = DateTime.Now;
         }
+        base.DisplayMsg("Well done!!");
 
         base.ActivityResetTime(true);
 
-        base.DisplayMsg("Breathing activity completed."); 
+        base.DisplayMsg($"You have completed another {seconds} seconds of Breathing Activity."); 
+
+        base.ActivityPause(5);
         
     } 
  
