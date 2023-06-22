f05d3fd686d501bf4c574903eb42889774ce7c96
Kasmodan1 <Kasmodan1@yahoo.com>
2023-06-22 16:19:24 -0600

    fixed for loop forgot {} brackets

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -2,11 +2,28 @@ using System;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
 
         Square square = new Square("Blue", 4);
-        
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("yellow", 2, 4);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("green", 4);
+        shapes.Add(circle);
+                                
+        foreach (Shape item in shapes)
+        {
+            string color = item.GetColor();
+
+            double area = item.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }   
 }
