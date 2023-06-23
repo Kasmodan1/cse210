c0604b418334708cc5c3a034434b104bee931ce3
Kasmodan1 <Kasmodan1@yahoo.com>
2023-06-23 15:11:52 -0600

    uploading files and goal fn

---
+++ prove/Develop05/Goals.cs
@@ -0,0 +1,54 @@
+using System;
+
+class Goal
+{   
+    protected int _score;
+    protected Boolean _completed;
+    protected string _name;
+    protected List<string> _goal;
+
+  
+    
+    protected Goal(string name)
+    {
+
+    }
+
+    protected string GetName(string[])
+    {
+
+    }
+
+
+    protected Boolean IsGoalComplet()
+    {
+
+    }
+
+    protected int GetPoints()
+    {
+
+    }
+
+    protected void SetCompleted(Boolean completed)
+    {
+
+    }
+    protected int CalculatePoints()
+    {
+
+    }
+    protected void RecordEvent()
+    {
+
+    }
+    protected int GetScore()
+    {
+
+    }
+    protected void ListGoals()
+    {
+        
+    }
+
+}
