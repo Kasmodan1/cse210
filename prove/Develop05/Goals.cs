3bcdf54f24374a207cbd24026b5f8564fe55f985
Kasmodan1 <Kasmodan1@yahoo.com>
2023-06-23 17:30:55 -0600

    filling in some of the goals fn.

--- prove/Develop05/Goals.cs
+++ prove/Develop05/Goals.cs
@@ -1,54 +1,59 @@
 using System;
+using System.Collections.Generic;
 
 class Goal
 {   
-    protected int _score;
+    protected int _points;
     protected Boolean _completed;
     protected string _name;
-    protected List<string> _goal;
+    protected List<string> _goals;
 
   
     
     protected Goal(string name)
     {
-
+        _name = name;
+        _completed = false;
+        _points = 0;
+        _goals = new List<string>();
     }
 
-    protected string GetName(string[])
+    protected string GetName()
     {
-
+        return _name;
     }
 
 
-    protected Boolean IsGoalComplet()
+    protected bool IsGoalComplete()
     {
-
+        return _completed;
     }
 
-    protected int GetPoints()
+    public int GetPoints()
     {
-
+        return _points;
     }
 
-    protected void SetCompleted(Boolean completed)
+    protected void SetCompleted(bool completed)
     {
-
+        _completed = completed;
     }
-    protected int CalculatePoints()
+    protected virtual int CalculatePoints()
     {
-
+        return 0;
     }
     protected void RecordEvent()
     {
-
+        // logic to record an event for the goal
     }
-    protected int GetScore()
+    protected int GetTotalScore()
     {
-
+        // implement logic for getting total score.
+        return 0;
     }
     protected void ListGoals()
     {
-        
+        // put logic to list goals
     }
 
 }
