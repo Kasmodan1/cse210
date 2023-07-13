63192c35f0561a008760f7ae5d4e313bb815f66a
Kasmodan1 <Kasmodan1@yahoo.com>
2023-07-13 01:07:49 -0700

    Updated Lib Mystery books

--- final/FinalProject/MysteryBook.cs
+++ final/FinalProject/MysteryBook.cs
@@ -5,20 +5,15 @@ class MysteryBook : FictionBook
     private string _detective;
     private string _crime;
 
-    public MysteryBook(string title, string author, long isbn, string genre, bool availability, int targetage)
+    public MysteryBook(string title, string author, long isbn, string genre, bool availability, int targetage) 
     : base(title, author, isbn, genre, availability, targetage)
-    {
-        
-    }
-
-    public MysteryBook(string title, string author, long isbn, string genre)
-    : base(title, author, isbn, genre)
-    {
+    {   
         Title = title;
         Author = author;
         ISBN = isbn;
         Genre = genre;
-        Availability = true; 
+        Availability = availability;
+        TargetAge = targetage;
     }
 
     public string Detective
@@ -43,8 +38,27 @@ class MysteryBook : FictionBook
         Console.WriteLine($"Availability: {(Availability ? "Available" : "Not available")}");
         Console.WriteLine($"Detective: {Detective}");
         Console.WriteLine($"Crime : {Crime}");
-        
-    
     }
 
+    public void SetDetective(string detective)
+    {
+        Detective = detective;
+    }
+
+    public void SetCrime(string crime)
+    {
+        Crime = crime;
+    }
+
+    public override void Borrow()
+    {
+        Availability = false;
+        Console.WriteLine($"Successfully Borrowed the {Genre}: {Title}");
+    }
+
+    public override void Return()
+    {
+        Availability = true;
+        Console.WriteLine($"Successfully Returned the {Genre}: {Title}");
+    }
 }
