8bebd57497b49128bd725e329b305ae39275f9e4
Kasmodan1 <Kasmodan1@yahoo.com>
2023-07-12 22:37:17 -0700

    updated Sf with now lib changes

--- final/FinalProject/ScienceFictionBook.cs
+++ final/FinalProject/ScienceFictionBook.cs
@@ -8,29 +8,60 @@ class ScienceFictionBook : FictionBook
     public ScienceFictionBook(string title, string author, long isbn, string genre, bool availability, int targetage)
     : base(title, author, isbn, genre, availability, targetage)
     {
-        Availability = availability;
-        TargetAge = targetage;
-    }
-
-    public ScienceFictionBook(string title, string author, long isbn, string genre)
-    : base(title, author, isbn, genre)
-    {
         Title = title;
         Author = author;
         ISBN = isbn;
         Genre = genre;
-        Availability = true; 
+        Availability = availability;
+        TargetAge = targetage;
     }
 
+    public string ScienceSettings
+    {
+        get { return _settings; }
+        private set {_settings = value; }
+    }
+
+    public string ScienceTechnology
+    {
+        get { return _technology; }
+        private set {_technology = value; }
+    }
 
     public override void DisplayBookDetails()
     {
-        // Implement the logic to display the details of a fiction book
+        // Implement the logic to display the details of a science fiction book
         Console.WriteLine($"Title: {Title}");
         Console.WriteLine($"Author: {Author}");
         Console.WriteLine($"ISBN: {ISBN}");
         Console.WriteLine($"Genre: {Genre}");
         Console.WriteLine($"Availability: {(Availability ? "Available" : "Not available")}");
         Console.WriteLine($"Target Age: {TargetAge}");
+         Console.WriteLine($"Setting: {ScienceSettings}");
+        Console.WriteLine($"Technology: {ScienceTechnology}");
+        
     }
+
+    public void SetScienceSettings(string settings)
+    {
+        ScienceSettings = settings;
+    }
+
+    public void SetScienceTechnology(string tech)
+    {
+        ScienceTechnology = tech;
+    }
+
+    public override void Borrow()
+    {
+        Availability = false;
+        Console.WriteLine($"Successfully Borrowed the {Genre}: {Title}");
+    }
+
+    public override void Return()
+    {
+        Availability = true;
+        Console.WriteLine($"Successfully Returned the {Genre}: {Title}");
+    }
+    
 }
